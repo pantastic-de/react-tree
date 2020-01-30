e45e63403808f8f2b852143dda22ff1ae784a7e5
Frank Steiner <frank.steiner@hpe.com>
2020-01-30 17:55:34 +0100

    Add CSS for Icons

--- src/Demo.js
+++ src/Demo.js
@@ -1,37 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import Tree, { TreeNode } from 'rc-tree';
-import 'rc-tree/assets/index.css';
-import './assets/index.less';
-import './assets/basic.less';
-import './assets/icon.less';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Tree, { TreeNode } from 'rc-tree'
+import './Demo.css'
 
 
 const treeData = [
-  { key: '0-0', title: 'parent 1', children:
+  { key: '0-0', title: 'Server', children:
     [
-      { key: '0-0-0', title: 'parent 1-1', children:
+      { key: '0-0-0', title: 'Proliant DL', children:
         [
-          { key: '0-0-0-0', title: 'parent 1-1-0' },
+          { key: '0-0-0-0', title: 'Gen6' },
+          { key: '0-0-0-1', title: 'Gen7' },
+          { key: '0-0-0-2', title: 'Gen8' },
+          { key: '0-0-0-3', title: 'Gen9' },
+          { key: '0-0-0-4', title: 'Gen10' },
         ],
       },
-      { key: '0-0-1', title: 'parent 1-2', children:
+      { key: '0-0-1', title: 'Blade', disableCheckbox: true, children:
           [
-            { key: '0-0-1-0', title: 'parent 1-2-0', disableCheckbox: true },
-            { key: '0-0-1-1', title: 'parent 1-2-1' },
+            { key: '0-0-1-0', title: 'BL460' },
+            { key: '0-0-1-1', title: 'BL6xx' },
           ],
       },
     ],
   },
-];
+]
 
 class Demo extends React.Component {
   static propTypes = {
     keys: PropTypes.array,
   };
   static defaultProps = {
-    keys: ['0-0-0-0'],
+    keys: ['0-0-1-0'],
   };
   constructor(props) {
     super(props);
@@ -52,7 +53,7 @@ class Demo extends React.Component {
     if (this.tree) {
       console.log(
       //  'Selected DOM node:',
-      //  selectedKeys.map(key => ReactDOM.findDOMNode(this.tree.domTreeNodes[key])),
+      selectedKeys.map(key => ReactDOM.findDOMNode(this.tree.domTreeNodes[key])),
       );
     }
   };
@@ -74,53 +75,15 @@ class Demo extends React.Component {
     this.tree = tree;
   };
   render() {
-    const customLabel = (
-      <span className="cus-label">
-        <span>operations: </span>
-        <span style={{ color: 'blue' }} onClick={this.onEdit}>Edit</span>&nbsp;
-        <label onClick={(e) => e.stopPropagation()}>
-          <input type="checkbox" /> checked
-        </label>
-        &nbsp;
-        <span style={{ color: '#EB0000' }} onClick={this.onDel}>Delete</span>
-      </span>
-    );
-
     return (
       <div style={{ margin: '0 20px' }}>
-        <h2>simple</h2>
-        <Tree
-          ref={this.setTreeRef}
-          className="myCls" showLine checkable defaultExpandAll
-          defaultExpandedKeys={this.state.defaultExpandedKeys}
-          onExpand={this.onExpand}
-          defaultSelectedKeys={this.state.defaultSelectedKeys}
-          defaultCheckedKeys={this.state.defaultCheckedKeys}
-          onSelect={this.onSelect} onCheck={this.onCheck}
-        >
-          <TreeNode title="parent 1" key="0-0">
-            <TreeNode title={customLabel} key="0-0-0">
-              <TreeNode title="leaf" key="0-0-0-0" style={{ background: 'rgba(255, 0, 0, 0.1)' }} />
-              <TreeNode title="leaf" key="0-0-0-1" />
-            </TreeNode>
-            <TreeNode title="parent 1-1" key="0-0-1">
-              <TreeNode title="parent 1-1-0" key="0-0-1-0" disableCheckbox />
-              <TreeNode title="parent 1-1-1" key="0-0-1-1" />
-            </TreeNode>
-            <TreeNode title="parent 1-2" key="0-0-2" disabled>
-              <TreeNode title="parent 1-2-0" key="0-0-2-0" checkable={false} />
-              <TreeNode title="parent 1-2-1" key="0-0-2-1" />
-            </TreeNode>
-          </TreeNode>
-        </Tree>
-
-        <h2>Check on Click TreeNode</h2>
         <Tree
           className="myCls"
           showLine
+          showIcon={false}
           checkable
           selectable={ false }
-          defaultExpandAll
+          defaultExpandAll={ true }
           onExpand={this.onExpand}
           defaultSelectedKeys={this.state.defaultSelectedKeys}
           defaultCheckedKeys={this.state.defaultCheckedKeys}
@@ -132,5 +95,4 @@ class Demo extends React.Component {
     );
   }
 }
-
-export default Demo;
+export default Demo
